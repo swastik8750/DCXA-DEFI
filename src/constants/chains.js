bd8df4498fcfd4a70e03b63ec7ec5d94a75903a3
Swastik8750 <Swastik8750@gmail.com>
2022-01-04 11:13:42 +0530

    swap and liquidity

---
+++ src/constants/chains.js
@@ -0,0 +1,21 @@
+export const networks = [3,4,5,42,444800, 444900]
+
+export const ChainId = {
+  // MAINNET: 1,
+  ROPSTEN: 3,
+  RINKEBY: 4,
+  GÖRLI: 5,
+  KOVAN: 42,
+  DEVNET: 444800,
+  AUTONITY: 444900,
+};
+
+export const routerAddress = new Map();
+// routerAddress.set(ChainId.MAINNET, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+routerAddress.set(ChainId.ROPSTEN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+routerAddress.set(ChainId.RINKEBY, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+routerAddress.set(ChainId.GÖRLI, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+routerAddress.set(ChainId.KOVAN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+routerAddress.set(ChainId.DEVNET, "0x04e555283D37aE85F6eB311fe2578F3B3f3dFc52");
+routerAddress.set(ChainId.AUTONITY, "0x4489D87C8440B19f11d63FA2246f943F492F3F5F");
+
---
+++ src/constants/coins.js
@@ -0,0 +1,154 @@
+import * as chains from './chains';
+
+// If you add coins for a new network, make sure Weth address (for the router you are using) is the first entry
+
+const AUTONITYCoins = [
+  {
+    name: "Auton",
+    abbr: "AUT",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Token A",
+    abbr: "TA",
+    address: "0x1d29BD2ACedBff15A59e946a4DE26d5257447727",
+  },
+  {
+    name: "Token B",
+    abbr: "TB",
+    address: "0xc108a13D00371520EbBeCc7DF5C8610C71F4FfbA",
+  },
+  {
+    name: "Token C",
+    abbr: "TC",
+    address: "0xC8E25055A4666F39179abE06d466F5A98423863F",
+  },
+  {
+    name: "Token D",
+    abbr: "TD",
+    address: "0x23238098F2B4dd9Ba3bb8bc78b639dD113da697e",
+  }
+]
+
+const DEVNETCoins = [
+  {
+    name: "Auton",
+    abbr: "AUT",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Newton",
+    abbr: "NEW",
+    address: "0xBd770416a3345F91E4B34576cb804a576fa48EB1",
+  },
+  {
+    name: "Token A",
+    abbr: "TA",
+    address: "0xD5073808CbA7d24500aa8f659B339b87945958a6",
+  },
+  {
+    name: "Token B",
+    abbr: "TB",
+    address: "0x908B8e60d149529d757F4aEd9320F246724f2b99",
+  }
+]
+
+const MAINNETCoins = [
+  {
+    name: "Ether",
+    abbr: "ETH",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Dai",
+    abbr: "DAI",
+    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+  },
+  {
+    name: "Tether USD",
+    abbr: "USDT",
+    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  },
+]
+
+const ROPSTENCoins = [
+  {
+    name: "Ether",
+    abbr: "ETH",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Dai",
+    abbr: "DAI",
+    address: "0xad6d458402f60fd3bd25163575031acdce07538d",
+  },
+  {
+    name: "Tether USD",
+    abbr: "USDT",
+    address: "0x6ee856ae55b6e1a249f04cd3b947141bc146273c",
+  },
+]
+
+const KOVANCoins = [
+  {
+    name: "Ether",
+    abbr: "ETH",
+    address: "", // // Weth address is fetched from the router
+  },
+  {
+    name: "Dai",
+    abbr: "DAI",
+    address: "0xc4375b7de8af5a38a93548eb8453a498222c4ff2",
+  },
+  {
+    name: "Tether USD",
+    abbr: "USDT",
+    address: "0xf3e0d7bf58c5d455d31ef1c2d5375904df525105",
+  },
+]
+
+const RINKEBYCoins = [
+  {
+    name: "Ether",
+    abbr: "ETH",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Dai",
+    abbr: "DAI",
+    address: "0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658",
+  },
+  {
+    name: "Tether USD",
+    abbr: "USDT",
+    address: "0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad",
+  },
+]
+
+const GÖRLICoins = [
+  {
+    name: "Ether",
+    abbr: "ETH",
+    address: "", // Weth address is fetched from the router
+  },
+  {
+    name: "Dai",
+    abbr: "DAI",
+    address: "0x73967c6a0904aa032c103b4104747e88c566b1a2",
+  },
+  {
+    name: "Tether USD",
+    abbr: "USDT",
+    address: "0x509ee0d083ddf8ac028f2a56731412edd63223b9",
+  },
+]
+
+const COINS = new Map();
+COINS.set(chains.ChainId.MAINNET, MAINNETCoins);
+COINS.set(chains.ChainId.ROPSTEN, ROPSTENCoins);
+COINS.set(chains.ChainId.RINKEBY, RINKEBYCoins);
+COINS.set(chains.ChainId.GÖRLI, GÖRLICoins);
+COINS.set(chains.ChainId.KOVAN, KOVANCoins);
+COINS.set(chains.ChainId.AUTONITY, AUTONITYCoins);
+COINS.set(chains.ChainId.DEVNET, DEVNETCoins);
+export default COINS
