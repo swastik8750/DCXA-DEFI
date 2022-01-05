20f19a419c424846077ecd90c00d3fb5f0ea9a23
Swastik8750 <Swastik8750@gmail.com>
2022-01-05 14:47:19 +0530

    remove gitignore files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
