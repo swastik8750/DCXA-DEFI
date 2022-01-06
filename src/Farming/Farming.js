e03cbc7ad89fb191262cf32e145005edbba82abc
nishantrajputnr <nishunishuraj18@gmail.com>
2022-01-06 13:05:54 +0530

    farming

--- src/Farming/Farming.js
+++ src/Farming/Farming.js
@@ -3,39 +3,63 @@ import './Farming.css';
 import { Button } from 'react-bootstrap';
 
 function Farming(props) {
-    return (
-        <>
-            {/* This is {props.name} page {props.bestway}<br /> */}
+    const header = props.user.header;
+    let content;
+    if (!header) 
+    {
+        content=
+        <div className="cont">
+                <div className="first according">
 
-            <div className="cont">
-                <div className="first">
-                    <div className='p-2 white'>SHDW-USDC</div>
+                    <div className='p-2 white'>{props.user.name}</div>
                     <div className='flex p-2'>
-                        <button className='first-btn m-1'>NEW</button>
-                        <button className="second-btn m-1"> DOUBLE TIP</button>
-                        <div className='bigger p-2'>
-                            <div className='design mb-2'>172.89%</div>
-                            <strike className="whiter">172.89%</strike>
-                        </div>
+                        <button className='according first-btn m-1'>NEW</button>
+                        <button className="according second-btn m-1"> DOUBLE TIP</button>
+
                     </div>
                     <div className='p-2 linkk'>
                         <a href='#'>Oracle Double Tip</a>
                     </div>
                     <div className='p-2 whiter'>
-                        TVL &nbsp;&nbsp;$682.92K
+                        TVL &nbsp;&nbsp;{props.user.tvl}
                     </div>
                 </div>
-                <div className="second center">
-                    <div className='p-2 whiter center'>Yield Farming : 464.15%</div>
-                    <div className='p-2 whiter center'>Trading Fee Income : 0%</div>
-                    <div className='p-2 whiter center'>Total APR : 34.6%</div>
-                    <div className='p-2 whiter center'>Total APR : 34.6%</div>
+                <div className="second center according">
+                    <div className='m-2'><p className='hint design'>{props.user.new}</p></div>
+                    <div><strike className="whiter">{props.user.previous}</strike></div>
                 </div>
-                <div className="third">adf</div>
-                <div className="fourth">
+                <div className="third center according">
+                    <div className='p-2 whiter center'>Yield Farming : {props.user.yield}</div>
+                    <div className='p-2 whiter center'>Trading Fee Income : {props.user.trading_fee_income}</div>
+                    <div className='p-2 whiter center'>Total APR : {props.user.total_apr}</div>
+                    <div className='p-2 whiter center'>Daily APR : {props.user.daily_apr}</div>
+                </div>
+                <div className="fourth according">
                     <button className="farm">Farm</button>
                 </div>
             </div>
+    }
+    else
+    {
+        content=
+        <div className="cont yellow change">
+                <div className="first according bigger">
+                    STRATEGY
+                </div>
+                <div className="second center according bigger">
+                   APY
+                </div>
+                <div className="third center according bigger">
+                   APR
+                </div>
+                <div className="fourth center according bigger">
+                    LEVERAGE
+                </div>
+            </div>
+    }
+    return (
+        <>
+            {content}
         </>
     );
 }
