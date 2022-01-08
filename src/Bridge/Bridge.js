2b3c1ded2d08ab9e4907d2207788331b6992b2bc
harshprabha <suraj365311@gmail.com>
2022-01-08 21:28:35 +0530

    bridge

--- src/Bridge/Bridge.js
+++ src/Bridge/Bridge.js
@@ -5,13 +5,30 @@ import Typography from '@mui/material/Typography';
 import './Bridge.css';
 import { FaInfo } from "react-icons/fa";
 import { TextField } from '@mui/material';
+import { makeStyles } from "@material-ui/core/styles";
 import { Button } from '@mui/material';
 import { grey } from '@mui/material/colors';
-
+const useStyles = makeStyles({
+    root: {
+    "& .MuiOutlinedInput-root" :{
+            color:"white"
+        },
+      "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+        borderColor: "white",
+      },
+      "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+        borderColor: "white",
+      },
+      "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+        borderColor: "white",
+      }
+    }
+  });
 
 export default function Bridge() {
+    const classes = useStyles();
     return (
-        <Paper className="mt-5 container_bridge mb-4" sx={{ pt: 10, pb: 10, pl: 2, pr: 2, margin: 'auto', maxWidth: 750, flexGrow: 1 }}>
+        <Paper className="mt-5 container_bridge mb-4" sx={{ pt: 10, pb: 10, pl: 2, pr: 2, margin: 'auto', maxWidth: 750, color:'White',backgroundColor:'rgba(0, 0, 0, 0.4)',boxShadow:2 }}>
             <div className="heading mb-4">BRIDGE</div>
             <Grid container spacing={6} className="pb-3 pr pt-3">
                 <Grid item className="mt-4">
@@ -25,7 +42,7 @@ export default function Bridge() {
                 </Grid>
                 <Grid item className="mt-4 ">
                     <Typography gutterBottom variant="subtitle1" component="div" >
-                        <TextField className="input" style={{ width: 200 }} size="small" id="outlined-basic" label="Amount" variant="outlined" placeholder='0' InputProps={{ endAdornment: <button className="send leftr">Transfer</button> }} />
+                        <TextField className={classes.root} sx={{width:200}} size="small" label="Amount"  InputLabelProps={{style:{color: 'white'},}}  variant="outlined" placeholder='0' InputProps={{ endAdornment: <button className="send leftr">Transfer</button> }} />
                     </Typography>
                 </Grid>
                 <Grid item className="mt-4">
