be87f6a315e2b965069e3626d0a632370d26893d
harshprabha <suraj365311@gmail.com>
2022-01-13 17:30:34 +0530

    animated

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -19,10 +19,7 @@ return (
         <div className="xyz" id="staking"><div><FcDeployment id="color" /></div><div>Staking</div></div>
         <div className="xyz" id="ifo"><div><FcFilingCabinet id="color" /></div><div>IFO</div></div>
         </div>
-        <Farm>
-            
-        </Farm>
-
+        <Farm/>
         <div className="unlockwallet">
                 <div className="wltone">
                     <FaWallet className='height' />
